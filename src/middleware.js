69472c8a08b9ddef5e59073c5f039ff7300dc7c1
Md Antor Mia <mdantormia1779@gmail.com>
2026-06-19 13:04:42 +0600

    middle ware fixed

--- src/middleware.js
+++ src/middleware.js
@@ -3,11 +3,10 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const { pathname } = request.nextUrl;
 
-  console.log("🔥 MIDDLEWARE:", pathname);
-
-  // ❌ শুধু admin protect
   if (pathname.startsWith("/admin")) {
-    const token = request.cookies.get("better-auth.session_token");
+    const token = request.cookies.get("better-auth.session_token")?.value;
+
+    console.log("TOKEN:", token);
 
     if (!token) {
       return NextResponse.redirect(new URL("/auth/login", request.url));
